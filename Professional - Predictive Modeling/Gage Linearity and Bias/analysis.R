f0169948ae1314493c8b859fee5168a78066a800
kram0275 <wpkram@gmail.com>
2019-06-11 20:20:47 -0500

    Completed OLS section

--- Professional - Predictive Modeling/Gage Linearity and Bias/analysis.R
+++ Professional - Predictive Modeling/Gage Linearity and Bias/analysis.R
@@ -2,15 +2,23 @@ rm(list = ls())
 df <- read.csv(file = "dummydata.csv")
 df <- df[ , -c(5:7)]
 colnames(df)[1:4] <- c("sample", "ref", "m1", "m2")
+rownames(df) <- df$sample
+df <- df[ , -1]
+
+df$ref[8:10] <- NA
+# str(df)
+
+
 df$bias1 <- df$m1 - df$ref
 df$bias2 <- df$m2 - df$ref
 
-rownames(df) <- df$sample
-df <- df[ , -1]
-df$ref[5:7] <- NA
-str(df)
-
 attach(df)
+
+m1.model <- lm(m1 ~ ref)
+summary(m1.model)
+
+m2.model <- lm(m2 ~ ref)
+summary(m2.model)
 
 b1.model <- lm(bias1 ~ ref)
 summary(b1.model)
@@ -26,3 +34,4 @@ ref.predict1 <- (p1.val - b1.model$coefficients[1]) /
                  
 ref.predict2 <- (p2.val - b2.model$coefficients[1]) / 
   (b2.model$coefficients[2] + 1) 
+
