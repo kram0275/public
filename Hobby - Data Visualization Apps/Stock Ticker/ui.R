58bcf92108ebc6ae711a202f93f969cd5bea3689
Will P. Kramlinger <37965763+kram0275@users.noreply.github.com>
2020-10-26 09:40:21 -0500

    Update action buttons. Temporarily remove download button.

--- Hobby - Data Visualization Apps/Stock Ticker/ui.R
+++ Hobby - Data Visualization Apps/Stock Ticker/ui.R
@@ -9,15 +9,15 @@ library(plotly)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Stock Ticker"),
+  titlePanel("Stock Performance"),
   
   # Sidebar 
   sidebarLayout(
     sidebarPanel(
       h4('Select a date range.'),
-      h4('Enter up to 5 stock symbols in the fields below, and press "LOCK
-         IN SELECTIONS".'),
-      h4('Press "PLOT IT" to, you know, plot it.'),
+      h4('Enter up to 5 stock symbols in the fields below, and press "1. Select 
+         Symbols".'),
+      h4('Press "2. Update Plot and Table" to, you know, plot it.'),
       
       dateRangeInput("thedates", "Select your date range.", 
                      start = "2020-01-01"),
@@ -26,9 +26,8 @@ shinyUI(fluidPage(
       textInput("stock3", "Enter a stock symbol.", "MSFT"),
       textInput("stock4", "Enter a stock symbol.", "GOOG"),
       textInput("stock5", "Enter a stock symbol.", "FB"),
-      actionButton("go", "LOCK IN SELECTIONS"),
-      actionButton("plotit", "PLOT IT"),
-      actionButton("download", "Download CSV"),
+      actionButton("go", "1. Select Symbols"),
+      actionButton("plotit", "2. Update Plot and Table"),
       h4("Will Kramlinger, 10/13/2020")
     ),
     
